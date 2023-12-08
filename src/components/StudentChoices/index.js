ca688820a72bd27af5b121f8ee586c50efe6562c
Sebastian Barcelona <sbarcelona@gmail.com>
2023-12-08 11:08:50 -0300

    testing error on vercel

--- src/components/StudentChoices/index.js
+++ src/components/StudentChoices/index.js
@@ -3,186 +3,191 @@ import React, { useState } from "react";
 import { Tabs, Tab } from "@/components/Common/Tabs";
 import Input from "@/components/Common/Input";
 import Chart from "@/components/Common/Charts";
-import Accordion from "@/components/Common/Accordion"
+import Accordion from "@/components/Common/Accordion";
 
-const ClassOverview = ({data}) => {
+const ClassOverview = ({ data }) => {
+  const {
+    social_skills = { low: 0, medium: 0, high: 0 },
+    emotional_management = { low: 0, medium: 0, high: 0 },
+    digital_wellbeing = { low: 0, medium: 0, high: 0 },
+  } = data;
 
-    const {
-        social_skills,
-        emotional_management,
-        digital_wellbeing
-    } = data
+  const social_skills_values = [
+    social_skills?.low,
+    social_skills?.medium,
+    social_skills?.high,
+  ];
 
+  const emotional_management_values = [
+    emotional_management?.low,
+    emotional_management?.medium,
+    emotional_management?.high,
+  ];
 
-    const social_skills_values = [
-        social_skills?.low || 0,
-        social_skills?.medium || 0,
-        social_skills?.high || 0
-    ]
+  const digital_wellbeing_values = [
+    digital_wellbeing?.low,
+    digital_wellbeing?.medium,
+    digital_wellbeing?.high,
+  ];
 
-    const emotional_management_values = [
-        emotional_management?.low || 0,
-        emotional_management?.medium || 0,
-        emotional_management?.high || 0
-    ]
-
-    const digital_wellbeing_values = [
-        digital_wellbeing?.low || 0,
-        digital_wellbeing?.medium || 0,
-        digital_wellbeing?.high || 0
-    ]
-
-    const options = {
-        chart: {
-            type: "donut",
-        },
-        colors: ["#4ABDAC", "#F7B733", "#FC4A1A"],
-        labels: ["Low", "Medium", "High"],
-        legend: {
-            show: false,
-            position: "bottom",
-        },
-        plotOptions: {
-            pie: {
-                donut: {
-                    size: 50,
-                    labels: {
-                        show: true,
-                        total: {
-                            show: false,
-                            showAlways: false,
-                            label: "Total",
-                            formatter: function (w) {
-                                return w.globals.seriesTotals.reduce((a, b) => {
-                                    return a + b
-                                }, 0) + "%"
-                            }
-                        }
-                    }
-                }
-            }
-        },
-        dataLabels: {
-            enabled: true,
-        },
-        responsive: [
-            {
-                breakpoint: 2600,
-                options: {
-                    chart: {
-                        width: 380,
-                    },
-                },
+  const options = {
+    chart: {
+      type: "donut",
+    },
+    colors: ["#4ABDAC", "#F7B733", "#FC4A1A"],
+    labels: ["Low", "Medium", "High"],
+    legend: {
+      show: false,
+      position: "bottom",
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          size: 50,
+          labels: {
+            show: true,
+            total: {
+              show: false,
+              showAlways: false,
+              label: "Total",
+              formatter: function (w) {
+                return (
+                  w.globals.seriesTotals.reduce((a, b) => {
+                    return a + b;
+                  }, 0) + "%"
+                );
+              },
             },
-            {
-                breakpoint: 640,
-                options: {
-                    chart: {
-                        width: 200,
-                    },
-                },
-            },
-        ],
-    };
+          },
+        },
+      },
+    },
+    dataLabels: {
+      enabled: true,
+    },
+    responsive: [
+      {
+        breakpoint: 2600,
+        options: {
+          chart: {
+            width: 380,
+          },
+        },
+      },
+      {
+        breakpoint: 640,
+        options: {
+          chart: {
+            width: 200,
+          },
+        },
+      },
+    ],
+  };
 
-    return (
-        <div>
-            <h3 className="m-3">Real Time Data</h3>
-            <div className="grid grid-col-1 md:grid-cols-12">
-                <div className="col-span-4">
-                    <Chart options={options} series={social_skills_values} type="donut"/>
-                    <div className="flex flex-col text-center">
-                        <h4 className="text-gray-400">Social Skills</h4>
-                        <p className="py-3 mx-6">“{social_skills.name || ''}”</p>
-                    </div>
-                </div>
-                <div className="col-span-4">
-                    <Chart options={options} series={emotional_management_values} type="donut"/>
-                    <div className="flex flex-col text-center">
-                        <h4 className="text-gray-400">Emotional Management</h4>
-                        <p className="py-3 mx-6">“{emotional_management.name || ''}”</p>
-                    </div>
-                </div>
-                <div className="col-span-4">
-                    <Chart options={options} series={digital_wellbeing_values} type="donut"/>
-                    <div className="flex flex-col text-center">
-                        <h4 className="text-gray-400">Digital Wellbeing</h4>
-                        <p className="py-3 mx-6">“{digital_wellbeing.name || ''}”</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <h3 className="m-3">Real Time Data</h3>
+      <div className="grid grid-col-1 md:grid-cols-12">
+        <div className="col-span-4">
+          <Chart options={options} series={social_skills_values} type="donut" />
+          <div className="flex flex-col text-center">
+            <h4 className="text-gray-400">Social Skills</h4>
+            <p className="py-3 mx-6">“{social_skills.name || ""}”</p>
+          </div>
         </div>
-    )
-}
+        <div className="col-span-4">
+          <Chart
+            options={options}
+            series={emotional_management_values}
+            type="donut"
+          />
+          <div className="flex flex-col text-center">
+            <h4 className="text-gray-400">Emotional Management</h4>
+            <p className="py-3 mx-6">“{emotional_management.name || ""}”</p>
+          </div>
+        </div>
+        <div className="col-span-4">
+          <Chart
+            options={options}
+            series={digital_wellbeing_values}
+            type="donut"
+          />
+          <div className="flex flex-col text-center">
+            <h4 className="text-gray-400">Digital Wellbeing</h4>
+            <p className="py-3 mx-6">“{digital_wellbeing.name || ""}”</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const StudentOverview = () => {
-    const comp1 = () => {
-        return (
-            <div>
-            </div>
-        )
-    }
-    const tabs = () => {
-        return (
-            <Tabs name="student">
-                <Tab component={comp1()}>Social Skills</Tab>
-                <Tab component={comp1()}>Emotional Management</Tab>
-                <Tab component={comp1()}>Healthy Tech Habits</Tab>
-            </Tabs>
-        )
-    }
-
-    const [accordions, setAccordion] = useState([
-        {
-            key: 1,
-            title: 'student 1',
-            data: tabs(),
-            isOpen: false
-        },
-        {
-            key: 2,
-            title: 'student 2',
-            data: tabs(),
-            isOpen: false
-        },
-        {
-            key: 3,
-            title: 'student 3',
-            data: tabs(),
-            isOpen: false
-        },
-    ]);
-
-    const toggleAccordion = (accordionkey) => {
-        const updatedAccordions = accordions.map((accord) => {
-            if (accord.key === accordionkey) {
-                return { ...accord, isOpen: !accord.isOpen };
-            } else {
-                return { ...accord, isOpen: false };
-            }
-        });
-
-        setAccordion(updatedAccordions);
-    };
-
+  const comp1 = () => {
+    return <div></div>;
+  };
+  const tabs = () => {
     return (
-        <div className="w-full">
-            <h3 className="m-3">Students (0)</h3>
-            <div className="inline-flex flex-col w-full">
-                {accordions.map((accordion) => (
-                    <Accordion
-                        key={accordion.key}
-                        title={accordion.title}
-                        data={accordion.data}
-                        isOpen={accordion.isOpen}
-                        toggleAccordion={() => toggleAccordion(accordion.key)}
-                    />
-                ))}
-            </div>
-            </div>
-    )
-}
-const StudentChoices = ({data}) => {
+      <Tabs name="student">
+        <Tab component={comp1()}>Social Skills</Tab>
+        <Tab component={comp1()}>Emotional Management</Tab>
+        <Tab component={comp1()}>Healthy Tech Habits</Tab>
+      </Tabs>
+    );
+  };
+
+  const [accordions, setAccordion] = useState([
+    {
+      key: 1,
+      title: "student 1",
+      data: tabs(),
+      isOpen: false,
+    },
+    {
+      key: 2,
+      title: "student 2",
+      data: tabs(),
+      isOpen: false,
+    },
+    {
+      key: 3,
+      title: "student 3",
+      data: tabs(),
+      isOpen: false,
+    },
+  ]);
+
+  const toggleAccordion = (accordionkey) => {
+    const updatedAccordions = accordions.map((accord) => {
+      if (accord.key === accordionkey) {
+        return { ...accord, isOpen: !accord.isOpen };
+      } else {
+        return { ...accord, isOpen: false };
+      }
+    });
+
+    setAccordion(updatedAccordions);
+  };
+
+  return (
+    <div className="w-full">
+      <h3 className="m-3">Students (0)</h3>
+      <div className="inline-flex flex-col w-full">
+        {accordions.map((accordion) => (
+          <Accordion
+            key={accordion.key}
+            title={accordion.title}
+            data={accordion.data}
+            isOpen={accordion.isOpen}
+            toggleAccordion={() => toggleAccordion(accordion.key)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+const StudentChoices = ({ data }) => {
   return (
     <div className="drop-shadow-lg gap-[15px] p-[30px] w-full flex bg-white flex-col justify-between">
       <div className="flex justify-center items-center w-full">
@@ -198,12 +203,12 @@ const StudentChoices = ({data}) => {
           </div>
           <div className="flex flex-col">
             <Tabs name="student">
-              <Tab component={ClassOverview({data})}>Class Overview</Tab>
+              <Tab component={ClassOverview({ data })}>Class Overview</Tab>
               <Tab component={StudentOverview()}>Individual Students</Tab>
             </Tabs>
           </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
